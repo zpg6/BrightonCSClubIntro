5e4debb2e3cda7d2fd8f5d39d4c0520f64c82cd2
Zach <zpg6@pitt.edu>
2021-04-16 13:20:27 -0400

    Initial Commit

---
+++ BrightonCSClubIntro/BrightonCSClubIntroApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrightonCSClubIntroApp.swift
+//  BrightonCSClubIntro
+//
+//  Created by Zachary Grimaldi on 4/16/21.
+//
+
+import SwiftUI
+
+@main
+struct BrightonCSClubIntroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
